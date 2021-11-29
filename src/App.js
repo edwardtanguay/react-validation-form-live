5da82b9011c4cae7f9c9e3e5abd5215c8e58d5a3
Edward Tanguay (hm.win) <edward@tanguay.info>
2021-11-29 11:26:20 +0100

    name and phone fields work

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
+import { useState } from 'react';
 import './App.scss';
 
 function App() {
-  return (
-    <div className="App">
-		Hello
-    </div>
-  );
+	const [name, setName] = useState('');
+	const [phone, setPhone] = useState('');
+	const [payload, setPayload] = useState({});
+
+	const handleName = (e) => {
+		let _name = e.target.value;
+		setName(_name);
+	}
+
+	const handlePhone = (e) => {
+		let _phone = e.target.value;
+		setPhone(_phone);
+	}
+
+	return (
+		<div className="App">
+			<form>
+				<fieldset>
+					<legend>Order Form</legend>
+
+					<div className="row">
+						<label htmlFor="name">Name</label>
+						<input type="text" id="name" value={name} onChange={handleName} />
+					</div>
+					<div className="note">required, maximum 10 characters</div>
+
+					<div className="row">
+						<label htmlFor="phone">Phone</label>
+						<input type="text" id="phone" value={phone} onChange={handlePhone} />
+					</div>
+					<div className="note">e.g. 555-333-2222</div>
+
+					<div className="buttonRow">
+						<button>Register</button>
+					</div>
+
+				</fieldset>
+			</form>
+		</div>
+	);
 }
 
 export default App;
